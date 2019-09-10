f7713033df0acc5bca7bcdad0cfa8f0a07bdb1d7
plyn85 <plyn99@gmail.com>
2019-09-10 16:42:38 +0100

    finaly jasmine tests

--- scripts/calc.js
+++ scripts/calc.js
@@ -1,10 +1,11 @@
+Calculator = function(){
+this.value = 0;
 
-function addition(numOne,numTwo){
-
- if(typeof(numOne) == 'number' && typeof(numTwo) == 'number' ){
-     return numOne + numTwo ;
- }else {
-   alert( 'Error!');
- }
 }
-
+Calculator.prototype.add = function(number){
+    if(typeof(number) == "number"){
+        this.value += number;
+    }else {
+        alert("Error!");
+    }
+}
